d0444b5d90444818c24c9b97f5a4616fd40a8319
Ice Lee <lb291700351@live.cn>
2018-05-21 18:10:16 +0800

    Add codes

---
+++ Assets/scripts/player/Bullect.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Bullect : MonoBehaviour {
+    public float Speed = 10f;
+
+    public int Power = 1;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    private void Update() {
+    }
+
+    private void FixedUpdate() {
+        transform.Translate(transform.up * Speed * Time.deltaTime, Space.World);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        //打击
+        other.SendMessage("hint", Power);
+        var objTag = other.tag;
+        switch (objTag) {
+            case "Wall":
+                other.SendMessage("hint", Power);
+                Destroy(gameObject);
+                break;
+            case "Steel":
+                other.SendMessage("hint", Power);
+                Destroy(gameObject);
+                break;
+            case "Heart":
+                other.SendMessage("hint", Power);
+                Destroy(gameObject);
+                break;
+            case "Grass":
+                Debug.Log("击中Grass");
+                break;
+            case "River":
+                Debug.Log("击中River");
+                break;
+            default:
+                Debug.Log("正常飞行");
+                break;
+        }
+    }
+}
---
+++ Assets/scripts/player/Player.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float Speed = 3f;
+
+//    public Sprite IconLeft;
+//    public Sprite IconUp;
+//    public Sprite IconRight;
+//    public Sprite IconDown;
+
+    public Transform ShootPosition; //子弹发射位置
+    public GameObject Bullect;
+
+    //方向
+    private Dir _dir = Dir.Up;
+
+
+    private SpriteRenderer _sp;
+
+
+    private bool _canShoot = true;
+
+    private Animator moveAnim;
+
+
+    private void Awake() {
+        _sp = GetComponent<SpriteRenderer>();
+
+        moveAnim = GetComponent<Animator>();
+    }
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+    }
+
+    private void FixedUpdate() {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Shoot();
+    }
+
+
+    private void Move(float h, float v) {
+        if (h != 0f || v != 0f) {
+            moveAnim.SetBool("isMoving",true);
+        }
+        else {
+            moveAnim.SetBool("isMoving",false);
+        }
+
+        //切换图片
+        if (h > 0) //right
+        {
+            transform.eulerAngles = new Vector3(0, 0, -90); //由欧拉角直接指定
+            //改变方向
+            _dir = Dir.Right;
+            //设置图片
+        }
+        else if (h < 0) {
+            _dir = Dir.Left;
+            transform.eulerAngles = new Vector3(0, 0, 90); //由欧拉角直接指定
+        }
+
+
+        if (v > 0) {
+            _dir = Dir.Up;
+            transform.eulerAngles = new Vector3(0, 0, 0); //由欧拉角直接指定
+        }
+        else if (v < 0) {
+            _dir = Dir.Down;
+            transform.eulerAngles = new Vector3(0, 0, 180); //由欧拉角直接指定
+        }
+
+        transform.Translate(Vector2.right * h * Speed * Time.deltaTime, Space.World);
+        transform.Translate(Vector2.up * v * Speed * Time.deltaTime, Space.World);
+    }
+
+    private void Shoot() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (_canShoot) {
+                Instantiate(Bullect, ShootPosition.position, ShootPosition.rotation);
+                _canShoot = false;
+                StartCoroutine(WaitShoot());
+            }
+        }
+    }
+
+    private enum Dir {
+        Left,
+        Up,
+        Right,
+        Down
+    }
+
+    private IEnumerator WaitShoot() {
+        yield return new WaitForSeconds(.45f);
+        _canShoot = true;
+    }
+}
